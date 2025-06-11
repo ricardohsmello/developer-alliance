d962fc5fa024435af95a62163d4c6cbd55981f97
Ricardo Mello <ricardohsmello@gmail.com>
2025-06-11 14:22:50 -0300

    including application starter

---
+++ src/main/java/com/alliance/AllianceSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.alliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllianceSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AllianceSpringBootApplication.class, args);
+	}
+
+}
